fff5ec5428e5d4e6d2742d080b3889ce6b877142
jo <whtkdgml36271@gmail.com>
2023-06-26 22:49:42 +0900

    0626 boardreply setting

---
+++ src/main/java/org/zerock/breply/BreplyApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.breply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/breply/BreplyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.breply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
